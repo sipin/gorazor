4142e9ba82a845aac43323ed71e6445f5d666c43
Wuvist <wuvist@gmail.com>
2014-05-03 23:46:00 +0800

    remove code

--- vash.js
+++ vash.js
@@ -1890,54 +1890,6 @@ var compileSingleHelper = function(str, options){
 	}
 
 }());
-/*jshint strict:false, asi:true, laxcomma:true, laxbreak:true, boss:true, curly:true, node:true, browser:true, devel:true */
-;(function(){
-
-	var helpers = vash.helpers;
-
-	// Trim whitespace from the start and end of a string
-	helpers.trim = function(val){
-		return val.replace(/^\s*|\s*$/g, '');
-	}
-
-	///////////////////////////////////////////////////////////////////////////
-	// EXAMPLE HELPER: syntax highlighting
-
-	helpers.config.highlighter = null;
-
-	helpers.highlight = function(lang, cb){
-
-		// context (this) is and instance of Helpers, aka a rendering context
-
-		// mark() returns an internal `Mark` object
-		// Use it to easily capture output...
-		var startMark = this.buffer.mark();
-
-		// cb() is simply a user-defined function. It could (and should) contain
-		// buffer additions, so we call it...
-		cb();
-
-		// ... and then use fromMark() to grab the output added by cb().
-		var cbOutLines = this.buffer.fromMark(startMark);
-
-		// The internal buffer should now be back to where it was before this
-		// helper started, and the output is completely contained within cbOutLines.
-
-		this.buffer.push( '<pre><code>' );
-
-		if( helpers.config.highlighter ){
-			this.buffer.push( helpers.config.highlighter(lang, cbOutLines.join('')).value );
-		} else {
-			this.buffer.push( cbOutLines );
-		}
-
-		this.buffer.push( '</code></pre>' );
-
-		// returning is allowed, but could cause surprising effects. A return
-		// value will be directly added to the output directly following the above.
-	}
-
-}());
 ;(function(){
 
 
