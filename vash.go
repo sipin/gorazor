793b1b904fe1a8c96f6f42e060731655ad1aaf3a
yukang <yukang@atrenta.com>
2014-05-05 13:24:46 +0800

    simple version lexer

--- vash.go
+++ vash.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"regexp"
+	"bufio"
 	"strings"
 )
 
 const (
-	EMAIL = iota
-	AT_STAR_OPEN
+	AT = iota
+	ASSIGN_OPERATOR
+	AT_COLON
 	AT_STAR_CLOSE
+	AT_STAR_OPEN
+	BACKSLASH
+	BRACE_CLOSE
+	BRACE_OPEN
+	CONTENT
+	EMAIL
+	ESCAPED_QUOTE
+	FORWARD_SLASH
+	FUNCTION
+	HARD_PAREN_CLOSE
+	HARD_PAREN_OPEN
+	HTML_TAG_CLOSE
+	THML_TAG_OPEN
+	HTML_TAG_VOID_CLOSE
+	IDENTIFIER
+	KEYWORD
+	LOGICAL
+	NEWLINE
+	NUMERIC_CONTENT
+	OPERATOR
+	PAREN_CLOSE
+	PAREN_OPEN
+	PERIOD
+	SINGLE_QUOTE
+	DOUBLE_QUOTE
+	TEXT_TAG_CLOSE
+	TEXT_TAG_OPEN
+	WHITESPACE
 )
 
 type TokenMatch struct {
@@ -18,9 +49,36 @@ type TokenMatch struct {
 }
 
 var Tests = []TokenMatch{
-	TokenMatch{EMAIL, "([a-zA-Z0-9.%]+@[a-zA-Z0-9.\\-]+\\.(?:ca|co\\.uk|com|edu|net|org))\\b"},
-	TokenMatch{AT_STAR_OPEN, "@\\*"},
-	TokenMatch{AT_STAR_CLOSE, "(\\*@)"},
+	TokenMatch{EMAIL, `([a-zA-Z0-9.%]+@[a-zA-Z0-9.\-]+\.(?:ca|co\.uk|com|edu|net|org))\\b`},
+	TokenMatch{AT_STAR_OPEN, `@\*`},
+	TokenMatch{AT_STAR_CLOSE, `(\*@)`},
+        TokenMatch{AT_COLON, `(@\:)`},
+	TokenMatch{AT, `(@)`},
+        TokenMatch{PAREN_OPEN, `(\()`},
+        TokenMatch{PAREN_CLOSE, `(\))`},
+        TokenMatch{HARD_PAREN_OPEN, `(\[)`},
+        TokenMatch{HARD_PAREN_CLOSE, `(\])`},
+        TokenMatch{BRACE_OPEN, `(\{)`},
+        TokenMatch{BRACE_CLOSE, `(\})`},
+        TokenMatch{TEXT_TAG_OPEN, `(<text>)`},
+	TokenMatch{TEXT_TAG_CLOSE, `(<\/text>)`},
+	TokenMatch{HTML_TAG_CLOSE, `(<\/[^>@\\b]+?>)`},
+	TokenMatch{HTML_TAG_VOID_CLOSE, `(\/\s*>)`},
+        TokenMatch{PERIOD, `(\.)`},
+        TokenMatch{WHITESPACE, `(\s)`},
+        TokenMatch{FUNCTION, `(function)([\d\w])`},
+        TokenMatch{KEYWORD, `(case|do|else|section|for|func|goto|if|return|switch|try|var|while|with)([\d\w])`},
+        TokenMatch{IDENTIFIER, `([_$a-zA-Z\\xA0-\\uFFFF][_$a-zA-Z0-9\\xA0-\\uFFFF]*)`},
+        TokenMatch{FORWARD_SLASH, `(\/)`},
+        TokenMatch{OPERATOR, `(===|!==|==|!==|>>>|<<|>>|>=|<=|>|<|\+|-|\/|\*|\^|%|\:|\?)`},
+	TokenMatch{ASSIGN_OPERATOR, `(\|=|\^=|&=|>>>=|>>=|<<=|-=|\+=|%=|\/=|\*=|=)`},
+        TokenMatch{LOGICAL, `(&&|\|\||&|\||\^)`},
+        TokenMatch{ESCAPED_QUOTE, `(\\+['\"])`},
+	TokenMatch{BACKSLASH, `(\\)`},
+        TokenMatch{DOUBLE_QUOTE, `(\\")`},
+	TokenMatch{SINGLE_QUOTE, `(\')`},
+	TokenMatch{NUMERIC_CONTENT, `([0-9]+)`},
+        TokenMatch{CONTENT, `([^\s})@.]+?)`},
 }
 
 type Token struct {
@@ -57,7 +115,6 @@ func Lex(lexer *Lexer, text string) ([]Token, error) {
 				}
 				cache = append(cache, reg)
 			}
-
 			regexp := cache[idx]
 			found := regexp.FindIndex([]byte(left))
 			if found != nil {
@@ -81,5 +138,24 @@ func Lex(lexer *Lexer, text string) ([]Token, error) {
 }
 
 func main() {
-
+	buf := ""
+	file, _ := os.Open("./tpl/layout/base.gohtml")
+	reader := bufio.NewReader(file)
+	for {
+		byte, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		buf += byte
+	}
+	lex := &Lexer{ buf}
+	res, err := Lex(lex, buf)
+	fmt.Println("buf:", buf)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	for _, elem := range res {
+		fmt.Println(elem)
+	}
 }
