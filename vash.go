464eba252256d31d7d80836dbd74b67f0d375b51
chenyukang <yukang@atrenta.com>
2014-05-06 17:42:35 +0800

    begin parser

--- vash.go
+++ vash.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	AT = iota
+	UNDEF = iota
+	AT
 	ASSIGN_OPERATOR
 	AT_COLON
 	AT_STAR_CLOSE
@@ -24,8 +25,8 @@ const (
 	FUNCTION
 	HARD_PAREN_CLOSE
 	HARD_PAREN_OPEN
+        HTML_TAG_OPEN
 	HTML_TAG_CLOSE
-	THML_TAG_OPEN
 	HTML_TAG_VOID_CLOSE
 	IDENTIFIER
 	KEYWORD
@@ -63,9 +64,11 @@ var Tests = []TokenMatch{
         TokenMatch{BRACE_CLOSE, `(\})`},
         TokenMatch{TEXT_TAG_OPEN, `(<text>)`},
 	TokenMatch{TEXT_TAG_CLOSE, `(<\/text>)`},
+        TokenMatch{HTML_TAG_OPEN, `(<[a-zA-Z@]+?[^>]*?["a-zA-Z]*>)`},
 	TokenMatch{HTML_TAG_CLOSE, `(<\/[^>@\\b]+?>)`},
 	TokenMatch{HTML_TAG_VOID_CLOSE, `(\/\s*>)`},
         TokenMatch{PERIOD, `(\.)`},
+	TokenMatch{NEWLINE, `(\n)`},
         TokenMatch{WHITESPACE, `(\s)`},
         TokenMatch{FUNCTION, `(function)([\D\W])`},
         TokenMatch{KEYWORD, `(case|do|else|section|for|func|goto|if|return|switch|try|var|while|with)([\D\W])`},
@@ -82,15 +85,62 @@ var Tests = []TokenMatch{
         TokenMatch{CONTENT, `([^\s})@.]+?)`},
 }
 
+var TokenStr = []string{
+	"UNDEF",
+        "AT",
+        "ASSIGN_OPERATOR",
+        "AT_COLON",
+        "AT_STAR_CLOSE",
+        "AT_STAR_OPEN",
+        "BACKSLASH",
+        "BRACE_CLOSE",
+        "BRACE_OPEN",
+        "CONTENT",
+        "EMAIL",
+        "ESCAPED_QUOTE",
+        "FORWARD_SLASH",
+        "FUNCTION",
+        "HARD_PAREN_CLOSE",
+        "HARD_PAREN_OPEN",
+        "HTML_TAG_CLOSE",
+        "THML_TAG_OPEN",
+        "HTML_TAG_VOID_CLOSE",
+        "IDENTIFIER",
+        "KEYWORD",
+        "LOGICAL",
+        "NEWLINE",
+        "NUMERIC_CONTENT",
+        "OPERATOR",
+        "PAREN_CLOSE",
+        "PAREN_OPEN",
+        "PERIOD",
+        "SINGLE_QUOTE",
+        "DOUBLE_QUOTE",
+        "TEXT_TAG_CLOSE",
+        "TEXT_TAG_OPEN",
+        "WHITESPACE"}
+
 type Token struct {
 	Text  string
 	Type  int
-	Occ   int
-	Start int
+	Line  int
+	Pos   int
+}
+
+func (token Token)P() {
+	typeStr := TokenStr[token.Type]
+	textStr := token.Text
+	if textStr == "\n" {
+		textStr = "\\n"
+	}
+	fmt.Printf("Token: %-20s Location:(%-2d %-2d) Value: %s\n",
+		    typeStr, token.Line, token.Pos, textStr)
 }
 
 type Lexer struct {
-	File string
+	Text    string
+        Cache   [](*regexp.Regexp)
+	Types   []int
 }
 
 func LineAndPos(src string, pos int) (int, int) {
@@ -99,58 +149,115 @@ func LineAndPos(src string, pos int) (int, int) {
 	return lines, p
 }
 
-func Lex(lexer *Lexer, text string) ([]Token, error) {
+func (lexer *Lexer) TagOpen(text string) (string) {
+        regs := []string {
+		`([a-zA-Z0-9.%]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,4})\b`,
+		`(@)`,
+                `(\/\s*>)`}
+	res := text
+	for _, reg := range regs {
+		regc, err := regexp.Compile(reg)
+		if err != nil {
+			panic(err)
+		}
+		found := regc.FindIndex([]byte(text))
+		if found != nil {
+			res = res[found[1]:]
+		}
+	}
+	return res
+}
+
+
+func (lexer *Lexer) Scan() ([]Token, error) {
 	pos := 0
 	toks := []Token{}
-	cache := []*regexp.Regexp{}
-	for pos < len(text) {
+	for _, test := range Tests {
+		reg, err := regexp.Compile("^" + test.Regex)
+		if err != nil {
+			panic(err)
+		}
+		lexer.Cache = append(lexer.Cache, reg)
+		lexer.Types = append(lexer.Types, test.Type)
+	}
+
+        text := strings.Replace(lexer.Text, "\r\n", "\n", -1)
+	text = strings.Replace(lexer.Text, "\r", "\n", -1)
+        for pos < len(text) {
 		left := text[pos:]
 		match := false
 		length := 0
-		for idx, test := range Tests {
-			pattern, id := test.Regex, test.Type
-			if len(cache) < idx+1 {
-				reg, err := regexp.Compile("^" + pattern)
-				if err != nil {
-					panic(err)
-				}
-				cache = append(cache, reg)
-			}
-			regexp := cache[idx]
+		for idx, regexp := range lexer.Cache {
 			found := regexp.FindIndex([]byte(left))
 			if found != nil {
 				match = true
-				length = found[1] - found[0]
-				fmt.Printf("length: %d found[0]: %d found[1]: %d\n",
-					length, found[0], found[1])
 				line, pos := LineAndPos(text, pos)
-				tok := Token{left[found[0]:found[1]], id, line, pos}
+				tokenVal := left[found[0]:found[1]]
+				toType := lexer.Types[idx]
+				if toType == HTML_TAG_OPEN {
+					tokenVal = lexer.TagOpen(tokenVal)
+				}
+				tok := Token{tokenVal, toType, line, pos}
 				toks = append(toks, tok)
+				length = len(tokenVal)
 				break
 			}
 		}
 		if !match {
 			err_line, err_pos := LineAndPos(text, pos)
 			return toks, fmt.Errorf("%d:%d: Illegal character: %s",
-				err_line, err_pos, string(text[pos]))
-		} else {
-			pos += length
+				     err_line, err_pos, string(text[pos]))
 		}
+		pos += length
 	}
 	return toks, nil
 }
 
-
-func test() {
-	buffer := "casex case"
-	lex := &Lexer{ buffer }
-	res, _ := Lex(lex, buffer)
-	fmt.Println(res)
+type Ast struct {
+	Parent *Ast
+	Children []*Ast
+	Value    string
 }
+
+func (ast *Ast) AddChild(child *Ast) {
+	ast.Children = append(ast.Children, child)
+}
+
+type Parser struct {
+	root       Ast
+	tokens     []Token
+	preTokens  []Token
+	inComment  bool
+        curr       Token
+}
+
+func (parser *Parser)Run() (err error) {
+	if(parser == nil) {
+		return
+	}
+	parser.curr = Token{"UNDEF", UNDEF, 0, 0}
+	for {
+		parser.preTokens = append(parser.preTokens, parser.curr)
+		if (len(parser.tokens) == 0) {
+			break
+		}
+		parser.curr = parser.tokens[0]
+		parser.tokens = parser.tokens[1:]
+		fmt.Println("now: ", parser.curr)
+	}
+	return nil
+}
+
+// func test() {
+// 	buffer := "casex case"
+// 	lex := &Lexer{ buffer }
+// 	res, _ := Lex(lex, buffer)
+// 	fmt.Println(res)
+// }
 
 func main() {
 	buf := ""
-	file, _ := os.Open("./tpl/layout/base.gohtml")
+	file, _ := os.Open("./now/demo.gohtml")
 	reader := bufio.NewReader(file)
 	for {
 		byte, err := reader.ReadString('\n')
@@ -159,16 +266,16 @@ func main() {
 		}
 		buf += byte
 	}
-	lex := &Lexer{ buf}
-	res, err := Lex(lex, buf)
+	lex := &Lexer{buf, []*regexp.Regexp{}, []int{}}
+	res, err := lex.Scan()
 	fmt.Println("buf:", buf)
 	if err != nil {
 		fmt.Println("error:", err)
 		return
 	}
 	for _, elem := range res {
-		fmt.Println(elem)
+		elem.P()
+		//fmt.Println(elem)
 	}
 
-	test()
 }
