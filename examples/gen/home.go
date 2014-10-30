004ca18fee3e1bae7959265032e1f99e5612189d
Chen Yukang <moorekang@gmail.com>
2014-10-30 10:37:24 +0800

    fix examples for latest version

--- examples/gen/home.go
+++ examples/gen/home.go
@@ -10,11 +10,8 @@ import (
 
 func Home(totalMessage int, u *User) string {
 	var _buffer bytes.Buffer
-	_buffer.WriteString("\n\n\n")
 	_buffer.WriteString((helper.Header()))
-	_buffer.WriteString("\n")
 	_buffer.WriteString((helper.Msg(u)))
-	_buffer.WriteString("\n\n")
 	for i := 0; i < 2; i++ {
 		if totalMessage > 0 {
 			if totalMessage == 1 {
@@ -40,50 +37,25 @@ func Home(totalMessage int, u *User) string {
 
 		}
 	}
-	_buffer.WriteString("\n\n\n")
-	{
-		for i := 0; i < 2; i++ {
-			if totalMessage > 0 {
-				if totalMessage == 1 {
 
-					_buffer.WriteString("<p>")
-					_buffer.WriteString(gorazor.HTMLEscape(u.Name))
-					_buffer.WriteString(" has 1 message</p>")
+	for i := 0; i < 2; i++ {
+		if totalMessage > 0 {
+			if totalMessage == 1 {
 
-				} else {
+				_buffer.WriteString("<p>")
+				_buffer.WriteString(gorazor.HTMLEscape(u.Name))
+				_buffer.WriteString(" has 1 message</p>")
 
-					_buffer.WriteString("<p>")
-					_buffer.WriteString(gorazor.HTMLEscape(u.Name))
-					_buffer.WriteString(" has ")
-					_buffer.WriteString(gorazor.HTMLEscape(gorazor.Itoa(totalMessage)))
-					_buffer.WriteString(" messages</p>")
-
-				}
 			} else {
 
 				_buffer.WriteString("<p>")
 				_buffer.WriteString(gorazor.HTMLEscape(u.Name))
-				_buffer.WriteString(" has no messages</p>")
+				_buffer.WriteString(" has ")
+				_buffer.WriteString(gorazor.HTMLEscape(gorazor.Itoa(totalMessage)))
+				_buffer.WriteString(" messages</p>")
 
 			}
-		}
-	}
-	_buffer.WriteString("\n\n")
-	{
-		switch totalMessage {
-		case 1:
-
-			_buffer.WriteString("<p>")
-			_buffer.WriteString(gorazor.HTMLEscape(u.Name))
-			_buffer.WriteString(" has 1  message</p>")
-
-		case 2:
-
-			_buffer.WriteString("<p>")
-			_buffer.WriteString(gorazor.HTMLEscape(u.Name))
-			_buffer.WriteString(" has 2 messages</p>")
-
-		default:
+		} else {
 
 			_buffer.WriteString("<p>")
 			_buffer.WriteString(gorazor.HTMLEscape(u.Name))
@@ -91,9 +63,29 @@ func Home(totalMessage int, u *User) string {
 
 		}
 	}
-	_buffer.WriteString("\n\n")
+
+	switch totalMessage {
+	case 1:
+
+		_buffer.WriteString("<p>")
+		_buffer.WriteString(gorazor.HTMLEscape(u.Name))
+		_buffer.WriteString(" has 1  message</p>")
+
+	case 2:
+
+		_buffer.WriteString("<p>")
+		_buffer.WriteString(gorazor.HTMLEscape(u.Name))
+		_buffer.WriteString(" has 2 messages</p>")
+
+	default:
+
+		_buffer.WriteString("<p>")
+		_buffer.WriteString(gorazor.HTMLEscape(u.Name))
+		_buffer.WriteString(" has no messages</p>")
+
+	}
+
 	_buffer.WriteString((helper.Footer()))
-	_buffer.WriteString("\n\n")
 
 	title := func() string {
 		var _buffer bytes.Buffer
@@ -104,14 +96,12 @@ func Home(totalMessage int, u *User) string {
 
 		return _buffer.String()
 	}
-	_buffer.WriteString("\n\n")
 
 	side := func() string {
 		var _buffer bytes.Buffer
 
 		return _buffer.String()
 	}
-	_buffer.WriteString("\n")
 
 	return layout.Base(_buffer.String(), title(), side())
 }
--- examples/gen/layout/base.go
+++ examples/gen/layout/base.go
@@ -6,13 +6,13 @@ import (
 
 func Base(body string, title string, side string) string {
 	var _buffer bytes.Buffer
-	_buffer.WriteString("\n<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\" />\n")
+	_buffer.WriteString("\n<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\" />")
 	_buffer.WriteString((title))
 	_buffer.WriteString("\n</head>\n<body>\n<div>")
 	_buffer.WriteString((body))
 	_buffer.WriteString("</div>\n<div>")
 	_buffer.WriteString((side))
-	_buffer.WriteString("</div>\n</body>\n</html>\n")
+	_buffer.WriteString("</div>\n</body>\n</html>")
 
 	return _buffer.String()
 }
