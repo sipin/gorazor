6a426e3bfcb6aa9c7d8e60675b9a173a96b42c33
Weng Wei <wuvist@gmail.com>
2019-06-18 15:45:26 +0800

    Add RazorQuick benchmarks

--- tests/bench_test.go
+++ tests/bench_test.go
@@ -2,11 +2,14 @@ package tests
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
+	"unsafe"
 
 	"github.com/sipin/gorazor/tests/data"
 	"github.com/sipin/gorazor/tests/tpl"
+	"github.com/valyala/quicktemplate"
 )
 
 func BenchmarkRazorTemplate1(b *testing.B) {
@@ -24,17 +27,55 @@ func BenchmarkRazorTemplate100(b *testing.B) {
 func benchmarkRazorTemplate(b *testing.B, rowsCount int) {
 	rows := getBenchRows(rowsCount)
 	b.RunParallel(func(pb *testing.PB) {
-		// bb := quicktemplate.AcquireByteBuffer()
-		// for pb.Next() {
-		// 	templates.WriteBenchPage(bb, rows)
-		// 	bb.Reset()
-		// }
-		// quicktemplate.ReleaseByteBuffer(bb)
 		for pb.Next() {
 			var sb strings.Builder
 			tpl.RenderIndex(&sb, rows)
 		}
 	})
+}
+
+func BenchmarkRazorQuickTemplate1(b *testing.B) {
+	benchmarkRazorQuickTemplate(b, 1)
+}
+
+func BenchmarkRazorQuickTemplate10(b *testing.B) {
+	benchmarkRazorQuickTemplate(b, 10)
+}
+
+func BenchmarkRazorQuickTemplate100(b *testing.B) {
+	benchmarkRazorQuickTemplate(b, 100)
+}
+
+func benchmarkRazorQuickTemplate(b *testing.B, rowsCount int) {
+	rows := getBenchRows(rowsCount)
+
+	b.RunParallel(func(pb *testing.PB) {
+		bb := quicktemplate.AcquireByteBuffer()
+		q := &quickStringWriter{bb: bb}
+		for pb.Next() {
+			tpl.RenderIndex(q, rows)
+			bb.Reset()
+		}
+		quicktemplate.ReleaseByteBuffer(bb)
+	})
+}
+
+func unsafeStrToBytes(s string) []byte {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&bh))
+}
+
+type quickStringWriter struct {
+	bb *quicktemplate.ByteBuffer
+}
+
+func (q *quickStringWriter) WriteString(s string) (int, error) {
+	return q.bb.Write(unsafeStrToBytes(s))
 }
 
 func getBenchRows(n int) []data.BenchRow {
