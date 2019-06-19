e04f0821e299691e3aa546ade66ead5427663016
Weng Wei <wuvist@gmail.com>
2019-06-19 14:15:30 +0800

    Update test cases

--- gorazor/test/badtag.go
+++ gorazor/test/badtag.go
@@ -20,24 +20,24 @@ func RenderBadtag(_buffer io.StringWriter, w *gorazor.Widget) {
 	if w.ErrorMsg != "" {
 
 		_buffer.WriteString("<div class=\"form-group has-error\">\n\t<div class=\"alert alert-danger\">")
-		_buffer.WriteString(gorazor.HTMLEscape(w.ErrorMsg))
+		_buffer.WriteString(gorazor.HTMLEscStr(w.ErrorMsg))
 		_buffer.WriteString("</div>")
 	} else {
 
 		_buffer.WriteString("<div class=\"form-group\">")
 	}
 	_buffer.WriteString("\n\n\t<label for=\"")
-	_buffer.WriteString(gorazor.HTMLEscape(w.Name))
+	_buffer.WriteString(gorazor.HTMLEscStr(w.Name))
 	_buffer.WriteString("\">")
-	_buffer.WriteString(gorazor.HTMLEscape(w.Label))
+	_buffer.WriteString(gorazor.HTMLEscStr(w.Label))
 	_buffer.WriteString("</label>\n\t<input type=\"text\" name=\"")
-	_buffer.WriteString(gorazor.HTMLEscape(w.Name))
+	_buffer.WriteString(gorazor.HTMLEscStr(w.Name))
 	_buffer.WriteString("\" class=\"form-control\" id=\"")
-	_buffer.WriteString(gorazor.HTMLEscape(w.Name))
+	_buffer.WriteString(gorazor.HTMLEscStr(w.Name))
 	_buffer.WriteString("\" placeholder=\"")
-	_buffer.WriteString(gorazor.HTMLEscape(w.PlaceHolder))
+	_buffer.WriteString(gorazor.HTMLEscStr(w.PlaceHolder))
 	_buffer.WriteString("\" value=\"")
-	_buffer.WriteString(gorazor.HTMLEscape(w.Value))
+	_buffer.WriteString(gorazor.HTMLEscStr(w.Value))
 	_buffer.WriteString("\">\n</div>")
 
 }
--- gorazor/test/layout.go
+++ gorazor/test/layout.go
@@ -18,11 +18,11 @@ func Layout(body string, title string, side string) string {
 
 func RenderLayout(_buffer io.StringWriter, body string, title string, side string) {
 	_buffer.WriteString("\n<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\" />")
-	_buffer.WriteString(gorazor.HTMLEscape(title))
+	_buffer.WriteString(gorazor.HTMLEscStr(title))
 	_buffer.WriteString("\n</head>\n<body>\n<div>")
-	_buffer.WriteString(gorazor.HTMLEscape(body))
+	_buffer.WriteString(gorazor.HTMLEscStr(body))
 	_buffer.WriteString("</div>\n<div>")
-	_buffer.WriteString(gorazor.HTMLEscape(side))
+	_buffer.WriteString(gorazor.HTMLEscStr(side))
 	_buffer.WriteString("</div>\n</body>\n</html>")
 
 }
